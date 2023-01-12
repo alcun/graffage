673713137251cf1ad7829c2bda65528a7ceb76df
alcun <alasdair@talkwell.world>
2023-01-12 01:00:43 +0000

    data fetch template

--- app/[graphType]/page.js
+++ app/[graphType]/page.js
@@ -1,11 +1,28 @@
 import React from "react";
 import GraphWrapper from "../../components/GraphWrapper/GraphWrapper";
 
-const Page = ({ params }) => {
+const Page = async ({ params }) => {
   const graphType = params.graphType;
+  async function getData() {
+    const res = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+  
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }
+  
+    return res.json();
+  }
+
+
+  const data = await getData();
   return (
     <>
       <GraphWrapper graphType={graphType} />
+      {JSON.stringify(data)}
     </>
   );
 };
--- components/GraphTypes/Bar/Bar.js
+++ components/GraphTypes/Bar/Bar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
+
 const Bar = () => {
+
+
+
   return (
     <div>Bar
-
     </div>
   )
 }
