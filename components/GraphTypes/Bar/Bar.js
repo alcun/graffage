45408e2316944bf9b9aea5fe155828d926d4dd22
alcun <alasdair@talkwell.world>
2023-01-12 18:19:08 +0000

    drawBar in function

--- components/GraphTypes/Bar/Bar.js
+++ components/GraphTypes/Bar/Bar.js
@@ -1,58 +1,15 @@
 import React from 'react'
 import * as d3 from "d3";
+import { drawBar } from './drawBar';
 
-function drawChart(svgRef) {
-  const data = [12, 5, 6, 6, 9, 10];
-  const h = 120;
-  const w = 250;
-  const svg = d3.select(svgRef.current);
 
-  svg
-    .attr("width", w)
-    .attr("height", h)
-    .style("margin-top", 50)
-    .style("margin-left", 50);
-
-  svg
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 40)
-    .attr("y", (d, i) => h - 10 * d)
-    .attr("width", 20)
-    .attr("height", (d, i) => d * 10)
-    .attr("fill", "steelblue");
-}
 
 
 const Bar = (svgRef) => {
-
-  const data = [12, 5, 6, 6, 9, 10];
-  const h = 120;
-  const w = 250;
-  const svg = d3.select(svgRef.current);
-
-  svg
-    .attr("width", w)
-    .attr("height", h)
-    .style("margin-top", 50)
-    .style("margin-left", 50);
-
-  svg
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 40)
-    .attr("y", (d, i) => h - 10 * d)
-    .attr("width", 20)
-    .attr("height", (d, i) => d * 10)
-    .attr("fill", "steelblue");
-
+  const svg = React.useRef(svgRef);
  
     React.useEffect(() => {
-      drawChart(svg);
+      drawBar(svg);
     }, [svg]);
   
     return (
